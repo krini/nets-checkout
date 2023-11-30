fd28df202cff9ddb7c11c0029037ec2a5132afc0
Kristoffer Kofoed <kristoffervn@gmail.com>
2023-11-30 11:44:57 +0100

    update to working ios example

--- EasyAPI.js
+++ EasyAPI.js
@@ -5,106 +5,101 @@
 import moment from 'moment';
 
 export default class EasyAPI {
+  constructor() {
+    // A redirect URL used to identify navigation
+    // from Mia SDK interface to the application.
+    //
+    // Note: Pass the same `redirectURL` for
+    // payment registration with Easy API and
+    // when presenting Mia SDK following payment registration.
+    this.redirectURL = 'https://127.0.0.1/redirect.php';
 
-    constructor() {
-        // A redirect URL used to identify navigation
-        // from Mia SDK interface to the application.
-        //
-        // Note: Pass the same `redirectURL` for
-        // payment registration with Easy API and
-        // when presenting Mia SDK following payment registration.
-        this.redirectURL = "https://127.0.0.1/redirect.php"
+    // Cancellation URL passed to EASY and the SDK to indentify
+    // user cancellation by using the "Go back" link rendered
+    // in the checkout webview.
+    // Note: Pass the same `cancelURL` for
+    // payment registration with Easy API and
+    // when presenting Mia SDK following payment registration.
+    this.cancelURL = 'https://cancellation-identifier-url';
 
-        // Cancellation URL passed to EASY and the SDK to indentify
-        // user cancellation by using the "Go back" link rendered
-        // in the checkout webview.
-        // Note: Pass the same `cancelURL` for
-        // payment registration with Easy API and
-        // when presenting Mia SDK following payment registration.
-        this.cancelURL = "https://cancellation-identifier-url";
+    // Default currency
+    this.currency = 'SEK';
 
-        // Default currency
-        this.currency = "SEK"
+    this.secretKey = 'YOUR TEST SECRET KEY';
+  }
 
-        this.secretKey = "YOUR TEST SECRET KEY"
-    }
+  createPaymentWithRequestBody(requestBody, callback) {
+    // EASY test environment is used for this demo
+    const baseURL = 'https://test.api.dibspayment.eu/v1/';
 
-    createPaymentWithRequestBody(requestBody, callback) {
-        // EASY test environment is used for this demo
-        const baseURL = "https://test.api.dibspayment.eu/v1/"
+    fetch(baseURL + '/payments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Token ' + this.secretKey,
+      },
+      body: requestBody,
+    })
+      //     .then(response => {
+      //     console.log('response', response)
+      //
+      // })
+      .then(response => response.json())
+      .then(json => {
+        callback(json);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-        fetch(baseURL + '/payments', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Token ' + this.secretKey
-            },
-            body: requestBody,
-        })
-        //     .then(response => {
-        //     console.log('response', response)
-        //
-        // })
-            .then((response) => response.json())
-            .then((json) => {
-                callback(json)
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+  makeSubscriptionRequest(price) {
+    const paymentRequest = this.makePaymentRequest(price);
 
-    makeSubscriptionRequest(price) {
-        const paymentRequest = this.makePaymentRequest(price)
+    const utc = '+03:00'; // EEST timezone is used for the sample app
+    const subscriptionEndDate =
+      moment()
+        .local()
+        .utcOffset(utc)
+        .add(3, 'years')
+        .format('YYYY-MM-DDThh:mm:ss') + utc;
 
-        const utc = "+03:00" // EEST timezone is used for the sample app
-        const subscriptionEndDate = moment().local().utcOffset(utc).add(3, 'years').format('YYYY-MM-DDThh:mm:ss') + utc;
+    paymentRequest.subscription = {
+      endDate: subscriptionEndDate,
+      interval: 0,
+    };
 
-        paymentRequest["subscription"] = {
-            "endDate": subscriptionEndDate,
-            "interval": 0
-        };
+    return paymentRequest;
+  }
 
-        return paymentRequest
-    }
-
-    // Returns the JSON request body required to create
-    // payment with Easy API.
-    makePaymentRequest(price) {
-        const amount = price * 100 // cents to notes
-        return {
-            "checkout": {
-                "termsUrl": "http://localhost:8080/terms",
-                "cancelURL": this.cancelURL,
-                "returnURL": this.redirectURL,
-                "consumerType": {
-                    "supportedTypes": [
-                        "B2C",
-                        "B2B"
-                    ],
-                    "default": "B2C"
-                },
-                "integrationType": "HostedPaymentPage"
-            },
-            "order": {
-                "reference": "MiaSDK-iOS",
-                "currency": this.currency,
-                "amount": amount,
-                "items": [
-                    {
-                        "unit": "pcs",
-                        "netTotalAmount": amount,
-                        "taxAmount": 0,
-                        "grossTotalAmount": amount,
-                        "quantity": 1,
-                        "name": "Lightning Cable",
-                        "unitPrice": amount,
-                        "taxRate": 0,
-                        "reference": "MiaSDK-iOS"
-                    }
-                ]
-            },
-        }
-    }
-
+  // Returns the JSON request body required to create
+  // payment with Easy API.
+  makePaymentRequest(price) {
+    const amount = price * 100; // cents to notes
+    return {
+      checkout: {
+        termsUrl: 'http://localhost:8080/terms',
+        cancelURL: this.cancelURL,
+        returnURL: this.redirectURL,
+        integrationType: 'HostedPaymentPage',
+        merchantHandlesConsumerData: true,
+      },
+      order: {
+        reference: 'MiaSDK-iOS',
+        items: [
+          {
+            reference: 'MiaSDK-iOS',
+            name: 'Lightning Cable',
+            quantity: 1,
+            unit: 'pcs',
+            unitPrice: amount,
+            grossTotalAmount: amount,
+            netTotalAmount: amount,
+          },
+        ],
+        amount: amount,
+        currency: 'DKK',
+      },
+    };
+  }
 }
